711d2dc2c2d0762427c635746555ded50e668432
tomo-kday <tomo.takebuchi@kkday.com>
2023-08-14 14:50:27 +0900

    chore: product page stub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,76 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { SearchProduct } from "../../types/api/searchProduct";
+import { Routes } from "../../constants";
+import FavoriteButton from "./components/favoriteButton/favoriteButton";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
+// since this data is not cached, this function will fire on user’s request every time.
+const fetchData = async (): Promise<SearchProduct[]> => {
+	try {
+		const data = await fetch(
+			"https://gd.activityjapan.com/get_search/plans?uri=/search/okina",
+			{
+				cache: "no-store", // same as SSR
+			}
+		);
+		const res = data.json();
+		return res;
+	} catch (err) {
+		throw new Error("error happened on server");
+	}
+};
+
+export default async function Home() {
+	const products = await fetchData();
+	return (
+		<main className={styles.main}>
+			<section>
+				{products.map((item) => (
+					<div key={item.plan_id}>
+						{/* header */}
+						<a href={`${Routes.SITE.BASEURL}/publish/plan/${item.plan_id}`}>
+							{item.plan_name}
+						</a>
+						<div className={styles.leftContainer}>
+							<Image
+								src={item.image}
+								alt={item.plan_name}
+								className={styles.image}
+								width={265}
+								height={198}
+								priority
+							/>
+							<FavoriteButton />
+						</div>
+						<div className={styles.rightComponent}>
+							{/* should come from data */}
+							<div className={styles.rightComponentHeader}>
+								青の洞窟・恩納村
+							</div>
+							<h2>{item.plan_name}</h2>
+							{item.review_point && (
+								<div className={styles.reviewContainer}>
+									<span>{item.review_point}</span>
+									<span>{item.review_count}</span>
+								</div>
+							)}
+
+							{/*  if ("review_point" in plan && plan["review_point"]) {
+          rating.innerHTML = `<span class="rating-sp__score"">${plan["review_point"]}</span><i class="icm icon-aj_star star-sp-rating" data-rate=${getReviewCount(plan["review_point"])}></i><span>(${plan['review_count']})</span>`;
+        } */}
+						</div>
+					</div>
+					// <Card key={item.plan_id} item={item} handleClick={handleClick} />
+				))}
+			</section>
+			{/* <Image
               src="/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
               height={24}
               priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            /> */}
+		</main>
+	);
 }
