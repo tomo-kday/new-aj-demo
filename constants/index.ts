d3d14bbadc7fb2462dcb4693328ed725671e1267
tomo-kday <tomo.takebuchi@kkday.com>
2023-08-17 13:42:16 +0900

    setup: constants

---
+++ constants/index.ts
@@ -0,0 +1,11 @@
+export const Routes = {
+	SITE: {
+		BASEURL: "https://activityjapan.com",
+		HOME: "/",
+	},
+	API: {
+		SEARCH: "/api/search/",
+	},
+} as const;
+
+export const weekList: string[] = ["日", "月", "火", "水", "木", "金", "土"];
