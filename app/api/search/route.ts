1fc51246ba9310275a898b1ff56fe2d7fe15844c
tomo-kday <tomo.takebuchi@kkday.com>
2023-08-17 13:50:56 +0900

    setup: mock API

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 // works as route
-export default async function GET(_req: NextApiRequest, res: NextApiResponse) {
+export default function GET(_req: NextApiRequest, res: NextApiResponse) {
 	res.status(200).json("okay");
 }
